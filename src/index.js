029e5bf098a887b989f4a90859070e0da81ce0a1
Katherine Lewis <kale8432@colorado.edu>
2019-07-03 10:46:55 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter
+} from 'react-router-dom;'
+import './index.css';
+import App from './App';
+
+const router = ( < BrowserRouter > < App / > < /BrowserRouter>)
+
+    ReactDOM.render(router, document.getElementById('root')); registerServiceWorker();
